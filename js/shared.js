50a3d87d7db4621e3730de797a896c2fde05ac74
yavorant <yavorant>
2021-06-27 17:53:25 +0300

    Last update

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,43 @@
 let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let selectPlanButtons = document.querySelectorAll('.plan button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
-//console.dir(backdrop);
 
-backdrop.style.display = 'block';
+//console.dir(selectPlanButtons);
+
+//backdrop.style.display = 'block';
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+backdrop.addEventListener("click", function() {
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+toggleButton.addEventListener("click", function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
+}
+
